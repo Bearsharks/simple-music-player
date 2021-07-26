3440536d2a10bbf1c69263185b217d6c8c1fa2d0
bearsharks <bearsharks02@gmail.com>
2021-07-26 14:14:50 +0900

    refact : 타입스크립트로

--- src/refs/keyGenerator.ts
+++ src/refs/keyGenerator.ts
@@ -1,6 +1,7 @@
-let curcnt = 0;
 
-export default function keyGenerator(cnt) {
+
+export default function keyGenerator(cnt: number): string | string[] {
+    let curcnt = 0;
     if (cnt) {
         let cur = Date.now();
         return new Array(cnt).fill(null).map((el, index) => cur + "_" + curcnt++);
