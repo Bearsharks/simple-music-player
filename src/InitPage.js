d45eb6697c0bf34ad2d322d886598824c7846b8d
bearsharks <bearsharks02@gmail.com>
2021-12-29 22:40:35 +0900

    test:axios cookie test

--- src/InitPage.js
+++ src/InitPage.js
@@ -1,13 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react"
 import Spinner from "./Spinner";
+import axios from "axios";
 const getProfileURL = `/main/profile`;
 function InitPage() {
     const navigate = useNavigate();
     useEffect(() => {
-        fetch(process.env.REACT_APP_API_URL + getProfileURL, {
-            credentials: 'include',
-            cache: 'no-cache'
+        axios(process.env.REACT_APP_API_URL + getProfileURL, {
+            withCredentials: true
         }).then((res) => {
             if (res.status === 403) throw new Error('403 is unacceptable for me!');
             return res.json();
