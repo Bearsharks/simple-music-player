5e27a1f694b6cf61b961031a0c8f30a6a5a40a40
bearsharks <bearsharks02@gmail.com>
2022-02-05 21:48:10 +0900

    feat: 유튜브에서 재생목록가져오는 기능 추가

--- src/refs/youtubeSearch.ts
+++ src/refs/youtubeSearch.ts
@@ -1,6 +1,9 @@
 import { getToken } from "./api";
 import { MusicInfo } from "./constants";
 
+export enum SearchType {
+    List, Music, Search, Playlist
+}
 function toMusicInfo(data: any, type: SearchType, query: string): MusicInfo {
     if (type === SearchType.List) {
         return {
@@ -25,19 +28,8 @@ function toMusicInfo(data: any, type: SearchType, query: string): MusicInfo {
         };
     }
 }
-export enum SearchType {
-    List, Music, Search
-}
+
 export default async function youtubeSearch(value: string, type: SearchType, pageToken?: string): Promise<MusicInfo[]> {
-    if (!sessionStorage.getItem("access_token")) {
-        try {
-            const token = await getToken();
-            sessionStorage.setItem("access_token", token);
-        } catch (e) {
-            console.error(e);
-            return [];
-        }
-    }
     let params: any;
     if (type === SearchType.List) {
         params = {
@@ -68,27 +60,56 @@ export default async function youtubeSearch(value: string, type: SearchType, pag
     }
     let query: string = Object.keys(params).map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k])).join('&');
     const getUrl = (type: SearchType, query: string) => {
-        let path = type === SearchType.List ? 'playlistItems' : (type === SearchType.Music) ? 'videos' : 'search';
+        let path = (type === SearchType.List) ? 'playlistItems' :
+            (type === SearchType.Music) ? 'videos' : 'search';
         return `https://www.googleapis.com/youtube/v3/${path}?${query}`;
     }
-    let res = await fetch(getUrl(type, query), { method: 'GET' });
+    let items = await YTFetch(getUrl(type, query));
+    let result: MusicInfo[] = [];
+    for (let item of items) {
+        result.push(toMusicInfo(item, type, value));
+    }
+    return result;
+}
+
+//파람만들기 파람으로 패스 만들기 결과 반환하기
+const YTFetch = async (url: string, pageToken?: string): Promise<any> => {
+    if (!sessionStorage.getItem("access_token")) {
+        try {
+            const token = await getToken();
+            sessionStorage.setItem("access_token", token);
+        } catch (e) {
+            console.error(e);
+            return [];
+        }
+    }
+
+    let res = await fetch(url);
     if (res.status === 200) {
         const data = await res.json();
-        let result = [];
-        for (let item of data.items) {
-            result.push(toMusicInfo(item, type, value));
+        if (data.nextPageToken) {
+            return data.items.concat(await YTFetch(url, data.nextPageToken));
         }
-        if (type === SearchType.List && data.nextPageToken) {
-            result = result.concat(await youtubeSearch(value, type, data.nextPageToken));
-        }
-        return result;
+        return data.items;
     } else if (res.status === 403 || res.status === 401) {
         //유튜브 읽기 권한이 없다면 무한루프가 발생하기 때문에 없다면 확인후 권한을 달라고 하자
         sessionStorage.setItem("access_token", "");
-        youtubeSearch(value, type, pageToken ? pageToken : undefined);
+        return YTFetch(url, pageToken);
     }
     else {
         alert('잘못된 키이거나 해당키의 api 할당량을 초과했습니다.');
     }
     throw new Error('request fail');
+}
+
+export const getMyYTPlaylist = async () => {
+    const params: any = {
+        access_token: sessionStorage.getItem("access_token"),
+        mine: true,
+        maxResults: 50,
+        fields: `nextPageToken,items(id,snippet(title,description,thumbnails,contentDetails))`
+    };
+    let query: string = Object.keys(params).map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k])).join('&');
+    let items = await YTFetch(`https://www.googleapis.com/youtube/v3/playlists?${query}`);
+    return items;
 }
