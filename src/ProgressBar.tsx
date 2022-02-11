1c84d525afb3cbf8ea901f26b7e3d5872dda2cce
bearsharks <bearsharks02@gmail.com>
2022-02-11 22:48:32 +0900

    feat:progressBar에 hover시 시간정보를 표시하는 기능추가

--- src/ProgressBar.tsx
+++ src/ProgressBar.tsx
@@ -1,39 +1,71 @@
-import React, { useEffect,useRef } from "react";
-import { MusicInfo, PlayerState } from "./refs/constants";
+import React, { useEffect, useRef } from "react";
+import { PlayerState } from "./refs/constants";
 import styles from "./ProgressBar.module.scss"
 import { useRecoilState } from "recoil";
 import { musicPlayerProgressState } from "./recoilStates/atoms/playlistAtoms";
 
-function ProgressBar(){
+function ProgressBar() {
     const thisElement = useRef<HTMLDivElement>(null);
+    const timeInfo = useRef<HTMLSpanElement>(null);
     const [progress, setProgress] = useRecoilState(musicPlayerProgressState);
-    useEffect(()=>{
-        const mytimer = setInterval(function() {
+    useEffect(() => {
+        const mytimer = setInterval(function () {
             const player = (window as any).player;
             if (player?.getPlayerState() === PlayerState.PLAYING ||
-              player?.getPlayerState() === PlayerState.PAUSED
+                player?.getPlayerState() === PlayerState.PAUSED
             ) {
-                setProgress({currentTime:player.getCurrentTime() ,duration : player.getDuration()});
-            }            
-        }, 1000); 
-        return ()=>{
+                setProgress({ currentTime: player.getCurrentTime(), duration: player.getDuration() });
+            }
+        }, 1000);
+        return () => {
             clearInterval(mytimer);
         }
-    },[]);
+    }, []);
 
-    useEffect(()=>{
-        const percent:number = (progress.currentTime / progress.duration) * 100;
-        if(thisElement.current){
-            thisElement.current.style.width = `${percent}vw`;
+    useEffect(() => {
+        const percent: number = (progress.currentTime / progress.duration) * 100;
+        if (thisElement.current) {
+            thisElement.current.style.width = `${percent}%`;
         }
-    },[progress]);
-    const progressClickHandler = (event:React.MouseEvent)=>{
-        const time = (event.clientX/document.body.offsetWidth) * progress.duration;
+    }, [progress]);
+
+    const progressClickHandler = (event: React.MouseEvent) => {
+        const time = (event.clientX / document.body.offsetWidth) * progress.duration;
         (window as any).player?.seekTo(time);
     }
-    return(
-        <div className={styles[`progress-bar`]} onClick={progressClickHandler}>
-            <div ref={thisElement} className={styles[`progress`]}></div>
+
+
+    let needForRAF = true;
+    const updateTimeInfo = (posX: number) => {
+        needForRAF = true;
+        if (timeInfo.current) {
+            const width = (document.body.offsetWidth - 12);
+            const time = (posX / width) * progress.duration;
+            const min = Math.floor(time / 60);
+            const sec = Math.round(time % 60);
+            const zero = (sec >= 10) ? "" : "0"
+            posX = posX < 10 ? 10 : posX;
+            posX = posX > width - 10 ? width - 10 : posX;
+            timeInfo.current.style.left = `${posX}px`;
+            timeInfo.current.textContent = `${min}:${zero}${sec}`;
+        }
+    }
+
+    const mm = (e: React.MouseEvent) => {
+        if (needForRAF) {
+            needForRAF = false;
+            requestAnimationFrame(() => updateTimeInfo(e.clientX));
+        }
+    }
+    return (
+        <div className={styles[`wrapper`]}
+            onClick={progressClickHandler}
+            onMouseMove={mm}>
+            <span ref={timeInfo}>4:38</span>
+            <div className={styles[`progress-bar`]} >
+                <div ref={thisElement} className={styles[`progress`]}></div>
+            </div>
+
         </div>
     )
 }
