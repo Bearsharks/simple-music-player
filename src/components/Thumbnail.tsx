45e7d89b5064fae46f00744a567bfbd531f528c2
bearsharks <bearsharks02@gmail.com>
2022-02-15 17:11:14 +0900

    fix: 재생목록 썸네일이  정사각형으로 표시되도록 수정

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -2,9 +2,9 @@ import styles from './Thumbnail.module.scss';
 
 function Thumbnail({ thumbnails, name }: { thumbnails: string[], name: string }) {
     const getImgs = (): string[] => {
+        if (thumbnails.length === 0) return [];
         let arr: string[] = thumbnails;
-        if (arr.length === 0) return ["https://lh3.googleusercontent.com/wr28amLh-pMk4vmrYv_Orhly8DTtdvZJFuLwmXG5RNvZJjGlFe_WMnKp4pWlZI1gL7ihQn-xZuzZ0A6VZZbv2Z-iTEH3dpjn=s576"];
-        else if (arr.length === 1) return arr;
+        if (arr.length === 1) return arr;
         else if (arr.length === 2) arr = [arr[0], arr[0], arr[1], arr[1]];
         while (arr.length < 4) {
             arr = arr.concat(arr);
@@ -13,11 +13,16 @@ function Thumbnail({ thumbnails, name }: { thumbnails: string[], name: string })
     }
     thumbnails = getImgs();
     return <div title={name} className={styles["wrapper"]}>
-        {thumbnails.length === 1 ?
-            <img src={thumbnails[0]} alt={""} style={{ "width": "100%", "height": "100%" }}></img> :
-            thumbnails.map((img: string, idx) => <div key={idx}
-                style={{ 'background': `url(${img}) -25px -10px` }}
-            ></div>)}
+        <img style={{ "width": "100%", "height": "100%" }} alt={""}
+            src={"https://lh3.googleusercontent.com/wr28amLh-pMk4vmrYv_Orhly8DTtdvZJFuLwmXG5RNvZJjGlFe_WMnKp4pWlZI1gL7ihQn-xZuzZ0A6VZZbv2Z-iTEH3dpjn=s576"}>
+        </img> {thumbnails.length &&
+            <div className={styles["img-wrap"]}>{
+                thumbnails.map((img: string, idx) => <img
+                    className={styles["img"]} key={idx} src={img} alt={""}
+                    style={thumbnails.length === 1 ? { "width": "100%", "height": "100%" } : {}}
+                ></img>)}
+            </div>
+        }
     </div>
 }
 
