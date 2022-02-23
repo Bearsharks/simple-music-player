9d774fc94858b78c14182afba2c868f4d9175276
bearsharks <bearsharks02@gmail.com>
2022-02-23 17:55:03 +0900

    test: 추가

--- src/__test__/musicList.test.tsx
+++ src/__test__/musicList.test.tsx
@@ -41,7 +41,7 @@ function useRecoilTest() {
 };
 
 describe('music list manager', () => {
-    it('LOAD_PLAYLIST', async () => {
+    it('LOAD APPEND ADD', async () => {
         const { result, waitForNextUpdate } = getRenderHook(useRecoilTest);
         if (!result.current) await waitForNextUpdate();
         const setAction: MusicListAction = {
@@ -52,12 +52,82 @@ describe('music list manager', () => {
             result.current.manager(setAction);
         })
         await waitForNextUpdate();
+        //load
         expect(result.current.musicList.length).toBe(playlistFixture.items.length);
-        for (let i = 0; i < playlistFixture.items.length; i++) {
-            expect(result.current.musicList[i].name).toBe(playlistFixture.items[i].name);
+        const appendAction: MusicListAction = {
+            type: MusicListActionType.APPEND_PLAYLIST,
+            payload: playlistFixture.id
+        }
+        act(() => {
+            result.current.manager(appendAction);
+        })
+        await waitForNextUpdate();
+        //append
+        expect(result.current.musicList.length).toBe(playlistFixture.items.length * 2);
+        for (let i = playlistFixture.items.length; i < playlistFixture.items.length * 2; i++) {
+            expect(result.current.musicList[i].name).toBe(playlistFixture.items[i - playlistFixture.items.length].name);
+        }
+        const addToNextAction: MusicListAction = {
+            type: MusicListActionType.ADD_TO_NEXT_PLAYLIST,
+            payload: playlistFixture.id
+        }
+        act(() => {
+            result.current.manager(addToNextAction);
+        })
+        await waitForNextUpdate();
+        //add to next
+        expect(result.current.musicList.length).toBe(playlistFixture.items.length * 3);
+        for (let i = 1; i <= playlistFixture.items.length; i++) {
+            expect(result.current.musicList[i].name).toBe(playlistFixture.items[i - 1].name);
+        }
+    });
+    it('APPEND ADD', async () => {
+        const { result, waitForNextUpdate } = getRenderHook(useRecoilTest);
+        if (!result.current) await waitForNextUpdate();
+        const appendAction: MusicListAction = {
+            type: MusicListActionType.APPEND_ITEMS,
+            payload: playlistFixture.items
+        }
+        act(() => {
+            result.current.manager(appendAction);
+        })
+        await waitForNextUpdate();
+        expect(result.current.musicList.length).toBe(playlistFixture.items.length);
+        const apptonext: MusicListAction = {
+            type: MusicListActionType.ADD_TO_NEXT,
+            payload: playlistFixture.items
+        }
+        act(() => {
+            result.current.manager(apptonext);
+        })
+        await waitForNextUpdate();
+        for (let i = 1; i <= playlistFixture.items.length; i++) {
+            expect(result.current.musicList[i].name).toBe(playlistFixture.items[i - 1].name);
         }
     });
 
+    it('delete', async () => {
+        const { result, waitForNextUpdate } = getRenderHook(useRecoilTest);
+        if (!result.current) await waitForNextUpdate();
+        const setAction: MusicListAction = {
+            type: MusicListActionType.LOAD_PLAYLIST,
+            payload: playlistFixture.id
+        }
+        act(() => {
+            result.current.manager(setAction);
+        })
+        await waitForNextUpdate();
+
+        const delAction: MusicListAction = {
+            type: MusicListActionType.DELETE,
+            payload: playlistFixture.items
+        }
+        act(() => {
+            result.current.manager(delAction);
+        })
+        await waitForNextUpdate();
+        expect(result.current.musicList.length).toBe(0);
+    });
 });
 
 
